5926b2d944550405413d70c824154e515f367154
James Bliss <james.bliss@astro73.com>
2013-01-23 21:18:40 -0500

    Mostly complete.

--- views.py
+++ views.py
@@ -1,11 +1,9 @@
 from django.template import loader, RequestContext
 from django.http import Http404, HttpResponse, HttpResponsePermanentRedirect
 from django.conf import settings
-from django.core.xheaders import populate_xheaders
 from django.utils.safestring import mark_safe
-from django.views.decorators.csrf import csrf_protect
 
-import yaml, markdown, codecs
+import markdown, codecs, os
 
 class StaticPage(object):
 	"""
@@ -15,50 +13,80 @@ class StaticPage(object):
 	meta = None
 	document = None
 	
-	def __init__(self, fn=None):
+	def __init__(self, fn=None, **kwargs):
 		"""StaticPage(string|file)
 		StaticPage()
 		
 		Loads data. First form from given file).
 		"""
+		self._title = None
 		if fn is not None:
 			if typeof(fn, basestring):
-				f = codecs.open(fn, mode="r", encoding="utf-8")
+				self._title = os.path.basename(fn)
+				f = codecs.open(fn, mode="rU", encoding="utf-8")
 			else:
 				f = fn
+				if hasattr(f, 'name')
+					self._title = os.path.basename(f.name)
+			
 			self.loadFile(f):
+		
+		if 'name' in kwargs:
+			self._title = os.path.basename(kwargs['name'])
+		if 'title' in kwargs:
+			self._title = kwargs['title']
 	
 	def loadFile(self, file):
 		"""sp.loadFile(file)
 		Loads data from the given file-like object.
 		"""
 		
-		lines = iter(file)
-		l = lines.next()
-		yamldata = ''
-		mddata = ''
-		if l.strip() == u'---':
-			for l in lines:
-				if l.strip() == u'---':
-					break
-				yamldata += l
-			l = lines.next()
-		
-		# At this point, l contains the first line of Markdown text
-		mddata = l
-		for l in lines:
-			mddata += l
-		
-		self.meta = yaml.load(yamldata)
-		self.text = mddata
-		self.document = markdown.markdown(self.text)
+		self.text = file.read()
+		md = markdown.Markdown(extensions=['meta']) #TODO: Add extensions as a configuration option
+		self.document = md.convert(self.text)
+		self.meta = md.Meta
+
+def findpage(url):
+	"""findpage(string) -> StaticPage
+	Returns the StaticPage for the given URL, or raises Http404
+	"""
+	if url[0] == '/':
+		url = url[1:]
+	
+	# Remove queries
+	if '?' in url:
+		i = url.find('?')
+		url = url[:i]
+	
+	# Just flat-out don't allow current directory or parent directory
+	if url[:2] == './'  or  '/./' in url or url[-2] == '/.':
+		raise Http404
+	if url[:3] == '../' or '/../' in url or url[-2] == '/..':
+		raise Http404
+	
+	for pagedir in config.STATICPAGES_DIRS:
+		fn = os.path.join(pagedir, url)
+		if os.path.exist(fn):
+			return StaticPage(fn)
+	else:
+		raise Http404
+	
 
 def render(request, url):
 	"""
 	Entry point for views.
 	"""
-	pass
+	page = findpage(url)
 	
-def findtemplate(url):
-	pass
-
+	c = RequestContext(request, {
+        'data': page.meta,
+        'text': mark_safe(page.document),
+    })
+    
+    if 'template' in page.meta:
+        t = loader.select_template((page.meta['template'], config.STATIC_TEMPLATE))
+    else:
+        t = loader.get_template(config.STATIC_TEMPLATE)
+    
+    response = HttpResponse(t.render(c))
+	return response
